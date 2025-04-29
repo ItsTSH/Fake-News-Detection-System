9a258b608bfeea0657b53102e57acb1503dc0ec4
TejinderSingh <tejindersingh0784@gmail.com>
2025-04-29 23:49:32 +0530

    tried changing the theme toggle

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -7,14 +7,23 @@ export const Header = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <header className="text-gray-600 body-font border-b-2 py-4 dark:text-slate-300 flex">
-      <div className = "container flex flex-wrap flex-col md:flex-row mx-6">
-        <div className = "flex title-font font-medium items-center text-gray-800 dark:text-slate-400">
-          <Link to="/" className = "ml-1 text-xl">Fake News Classification</Link>
+      <div className="container flex flex-wrap flex-col md:flex-row mx-6">
+        <div className="flex title-font font-medium items-center text-gray-800 dark:text-slate-400">
+          <Link to="/" className="ml-1 text-xl">
+            Fake News Classification
+          </Link>
         </div>
       </div>
-      <nav className = "md:ml-auto mr-6 flex items-center justify-end">
-        <Link to="/" className="mr-5 hover:text-gray-900 dark:hover:text-white">Home</Link>
-        <Link to="/about" className="mr-5 hover:text-gray-900 dark:hover:text-white">About</Link>
+      <nav className="md:ml-auto mr-6 flex items-center justify-end">
+        <Link to="/" className="mr-5 hover:text-gray-900 dark:hover:text-white">
+          Home
+        </Link>
+        <Link
+          to="/about"
+          className="mr-5 hover:text-gray-900 dark:hover:text-white"
+        >
+          About
+        </Link>
         <Button onClick={toggleTheme} variant = "themeSwitch">
           {theme === "dark" ? "☀️" : "🌙"}
         </Button>
